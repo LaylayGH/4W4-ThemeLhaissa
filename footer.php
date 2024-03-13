08628e529f4fad4f005473a4f9b8ebe2f18c9a6e
LaylayGH <lhaissa24@gmail.com>
2024-03-13 08:25:11 -0400

    Création de la branche tp-1

---
+++ footer.php
@@ -0,0 +1,8 @@
+	<div id="footer" class="global">
+		<section>
+			<h2>Footer</h2>
+			<h6>Tous droits réservés &copy;</h6>
+			<h6><a href="https://github.com/LaylayGH">Voici mon github!</a></h6>
+			<input type="button" value="Lakers ®️">
+		</section>
+	</div>
--- index.php
+++ index.php
@@ -51,14 +51,7 @@
 			</div>
 		</section>
 	</div>
-	<div id="footer" class="global">
-		<section>
-			<h2>Footer</h2>
-			<h6>Tous droits réservés &copy;</h6>
-			<h6><a href="https://github.com/LaylayGH">Voici mon github!</a></h6>
-			<input type="button" value="Lakers ®️">
-		</section>
-	</div>
+	<?php get_footer() ?>
 </body>
 
 </html>
