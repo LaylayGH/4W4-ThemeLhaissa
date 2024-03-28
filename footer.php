4539f10388f3704f48e8cf1439749ca2ffd31588
LaylayGH <lhaissa24@gmail.com>
2024-03-27 22:24:49 -0400

    Ajout d'animation dans les menus (catégories)

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 	<div id="footer" class="global bck-bleu-200 clr-bleu-300">
 		<section>
+		<h4>En savoir plus...</h4>
 			<input type="button" value="Voyage.inc ®️">
 			<h6>Tous droits réservés &copy;</h6>
 		</section>
